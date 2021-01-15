061b711419560e0ee949c2bd15b7fd5f31b61759
AlexanderIlin2005 <sashailyin2005@gmail.com>
2021-01-15 16:18:45 +0300

    added shooting добавлена обработка мыши и стрелок

--- main.py
+++ main.py
@@ -19,6 +19,35 @@ class Doctor(pygame.sprite.Sprite):
         hits = pygame.sprite.spritecollide(self, enemies, False)
         if hits:
             self.kill()
+        if args and args[0].type == pygame.MOUSEBUTTONDOWN and \
+                self.rect.collidepoint(args[0].pos):
+            vac = Vaccine(y=self.rect.centery)
+            vaccines.add(vac)
+        elif args and args[0].type == pygame.KEYDOWN:
+            print(args[0].key)
+            if args[0].key == 275:
+                self.rect.x += 150
+            if args[0].key == 276:
+                self.rect.x -= 150
+            if args[0].key == 274:
+                self.rect.y += 150
+            if args[0].key == 273:
+                self.rect.y -= 150
+
+
+class Vaccine(pygame.sprite.Sprite):
+
+    def __init__(self, *group, y):
+        super().__init__(*group)
+        self.image = load_image("/Users/alexeyilyin/Documents/шприц.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = 50
+        self.rect.centery = y
+
+    def update(self, *args):
+        self.rect.x += 5
+        if self.rect.x > 700:
+            self.kill()
 
 
 class Enemy(pygame.sprite.Sprite):
@@ -29,9 +58,17 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 700
         self.rect.y = 10
+        self.hp = 3
 
     def update(self, *args):
         self.rect.x -= 2
+        hits = pygame.sprite.spritecollide(self, vaccines, False)
+        if hits:
+            for hit in hits:
+                hit.kill()
+                self.hp -= damage
+        if self.hp <= 0:
+            self.kill()
 
 
 pygame.init()
@@ -40,6 +77,8 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Игра "Доктора против вирусов"')
 doctors = pygame.sprite.Group()
 enemies = pygame.sprite.Group()
+vaccines = pygame.sprite.Group()
+damage = 1
 
 for i in range(1):
     doc = Doctor()
@@ -50,16 +89,19 @@ fps = 60
 clock = pygame.time.Clock()
 running = True
 fr_c = 0
-
+rate_of_fire = 50
 while running:
     screen.fill((255, 255, 255))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    doctors.update()
+
+        doctors.update(event)
     enemies.update()
+    vaccines.update()
     doctors.draw(screen)
     enemies.draw(screen)
+    vaccines.draw(screen)
     clock.tick(fps)
     pygame.display.flip()
     fr_c += 1
