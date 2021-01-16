a02d33adac9c7f7a548e645ab4c1d5d508176918
AlexanderIlin2005 <sashailyin2005@gmail.com>
2021-01-16 20:36:49 +0300

    загружен файл шрифта. создано событие окончания игры. добавлены смайлики, которых надо защищать. добавлено 5 файлов:  - файл шрифта Phosphate.ttc  - 4 картинки для отображения смайликов: смайл1.png, смайл2.png, смайл3.png, больной смайлик.png

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 import pygame
 
@@ -9,7 +10,6 @@ def load_image(path):
 
 
 def write_exp_and_record(number):
-    font = pygame.font.Font(None, 40)
     text = font.render(f"Ваши очки: {number}", True, (0, 0, 255))
     text_x = 10
     text_y = 10
@@ -28,18 +28,52 @@ def draw_rects():
         pygame.draw.rect(screen, (195, 213, 229), rect)
 
 
+def do_gameover(details):
+    global running
+
+    def write_info():
+        global exp
+
+        texts = [font.render("Игра окончена", True, (0, 0, 255)), font.render(details, True, (0, 0, 255)),
+                 font.render(f"Результат: {exp}", True, (0, 0, 255))]
+        text_x = 300
+        text_y = 250
+        text_rect = pygame.Rect(text_x - 10, text_y - 10, 430, 145)
+        frame_rect = pygame.Rect(text_x - 15, text_y - 15, 435, 150)
+        pygame.draw.rect(screen, (200, 200, 200), text_rect)
+        pygame.draw.rect(screen, (180, 180, 180), frame_rect, 10)
+        screen.blit(texts[0], (text_x, text_y))
+        text_y += 40
+        screen.blit(texts[1], (text_x, text_y))
+        text_y += 40
+        screen.blit(texts[2], (text_x, text_y))
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                exit()
+        draw_rects()
+        enemies.draw(screen)
+        doctors.draw(screen)
+        smiles.draw(screen)
+        vaccines.draw(screen)
+        write_exp_and_record(exp)
+        write_info()
+        clock.tick(fps)
+        pygame.display.flip()
+
+
 class Doctor(pygame.sprite.Sprite):
 
     def __init__(self, *group):
         super().__init__(*group)
         self.image = load_image("data/doctor500.png")
         self.rect = self.image.get_rect()
-        self.rect.x = 200
+        self.rect.x = 100
         self.rect.y = 150
         self.move_rate = 120
 
     def update(self, *args):
-        hits = pygame.sprite.spritecollide(self, enemies, False)
         if args and args[0].type == pygame.MOUSEBUTTONDOWN and \
                 self.rect.collidepoint(args[0].pos):
             vac = Vaccine(y=self.rect.centery)
@@ -65,13 +99,39 @@ class Vaccine(pygame.sprite.Sprite):
         super().__init__(*group)
         self.image = load_image("data/shpritz.png")
         self.rect = self.image.get_rect()
-        self.rect.x = 270
+        self.rect.x = 150
         self.rect.centery = y - 2
 
     def update(self, *args):
         self.rect.x += 5
         if self.rect.x > 950:
             self.kill()
+
+
+class Smile(pygame.sprite.Sprite):
+
+    def __init__(self, *group, n):
+        super().__init__(*group)
+        self.image = load_image(f"data/смайл{random.choice([1, 2, 3])}.png")
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 50
+        self.rect.y = 160 + (120 * n)
+        self.hp = 2
+
+    def update(self, *args):
+        hits = pygame.sprite.spritecollide(self, enemies, False)
+        if hits:
+            y = hits[0].rect.y
+            self.hp -= 1
+            if self.hp == 0:
+                for hit in hits:
+                    hit.kill()
+                self.kill()
+            if self.hp == 1:
+                self.image = load_image("data/больной смайлик.png")
+                for en in enemies:
+                    if en.rect.y == y:
+                        en.kill()
 
 
 class Enemy(pygame.sprite.Sprite):
@@ -81,7 +141,7 @@ class Enemy(pygame.sprite.Sprite):
         self.image = load_image("data/virus1.png")
         self.images = [load_image("data/virus3.png"), load_image("data/virus5.png")]
         self.rect = self.image.get_rect()
-        self.rect.x = 900
+        self.rect.x = 800 + random.randint(-150, 150)
         self.rect.y = 150 + (120 * random.randint(0, 3))
         self.hp = 3
 
@@ -97,32 +157,40 @@ class Enemy(pygame.sprite.Sprite):
                     self.image = self.images[0]
                 if self.hp == 1:
                     self.image = self.images[1]
-        if self.rect.x < doc.rect.x + doc.rect.width and self.rect.y == doc.rect.y:
-            doc.kill()
+        if self.rect.x < 20:
+            do_gameover("Вирус пробрался!")
+        if self.rect.x < doc.rect.x + doc.rect.width - 30 and self.rect.y == doc.rect.y:
             if exp > record:
                 with open("record.txt", "w") as r_file:
                     r_file.write(f"{exp}")
+            do_gameover("Доктор заразился!")
         if self.hp <= 0:
             exp += 1
             self.kill()
 
 
 pygame.init()
+font = pygame.font.Font("data/Phosphate.ttc", 40)
 size = width, height = 1000, 600
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Игра "Доктора против вирусов"')
+pygame.display.set_caption('Игра "Вакцинация"')
 doctors = pygame.sprite.Group()
 enemies = pygame.sprite.Group()
 vaccines = pygame.sprite.Group()
+smiles = pygame.sprite.Group()
 damage = 1
 
 with open("data/record.txt", "r") as record_file:
     record = int(record_file.read())
 doc = Doctor()
 doctors.add(doc)
-for i in range(1):
+for i in range(10):
     enemy = Enemy()
     enemies.add(enemy)
+for i in range(4):
+    smile = Smile(n=i)
+    smiles.add(smile)
+
 fps = 60
 clock = pygame.time.Clock()
 running = True
@@ -137,14 +205,19 @@ while running:
         doctors.update(event)
     enemies.update()
     vaccines.update()
+    smiles.update()
     draw_rects()
     enemies.draw(screen)
     doctors.draw(screen)
+    smiles.draw(screen)
     write_exp_and_record(exp)
     vaccines.draw(screen)
     clock.tick(fps)
     pygame.display.flip()
     if fr_c % enemy_rate == 0:
-        en = Enemy()
-        enemies.add(en)
+        for i in range(1):
+            en = Enemy()
+            enemies.add(en)
     fr_c += 1
+    if not smiles:
+        do_gameover("Вирус победил!")
